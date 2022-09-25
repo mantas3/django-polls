b3a1742a491beb2849eb4096094aab1aea974134
mantas <mantas.armonavicius@gmail.com>
2022-09-25 11:57:30 +0300

    view fix

--- polls/views.py
+++ polls/views.py
@@ -18,7 +18,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+    
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -45,11 +50,3 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
-
-class DetailView(generic.DetailView):
-    ...
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
